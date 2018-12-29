f9c6269452232b83299d6295cee31156a3610dec
Meisuleen Kased <meisuleen@gmail.com>
2018-12-29 03:06:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,11 @@ class Triangle
     #if triangle_sides = (0, 0, 0) => raise TriangleError
     #if triangle_sides = (3, 5, -2) => raise TriangleError
     #if triangle_sides = (certain inequalities) => TriangleError
+    sum_one = @a + @b
+    sum_two = @a + @c 
+    sum_three = @b + @c 
+
+    if (@triangle_sides.none? {|side| side <= 0}) && (sum_one > @c && sum_two > @b && sum_three > @a)
   end
 
   def kind#check for the the side measurements and then return its type as a string
